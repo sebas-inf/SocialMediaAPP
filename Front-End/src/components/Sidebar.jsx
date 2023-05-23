6b43540d48c003c3803e40d5a59a15b69c67faeb
yukaymmt <yamamtyuka@gmail.com>
2023-05-23 19:17:59 -0400

    updated

--- Front-End/src/components/Sidebar.jsx
+++ Front-End/src/components/Sidebar.jsx
@@ -3,11 +3,6 @@ import {HiMenu} from 'react-icons/hi';
 import {AiFillCloseCircle} from 'react-icons/ai';
 import {Link, Route, Routes} from 'react-router-dom';
 
-import Sidebar from '../components/Sidebar';
-import UserProfile from '../components/UserProfile';
-import {client} from '../client';
-import logo from '..assets/logo.png';
-
 const Home = () => {
   return (
     <div>Home</div>
