7a058c1bf5c7ca8d5ca35b5e7b5a3110a433e871
yukaymmt <yamamtyuka@gmail.com>
2023-05-23 18:49:03 -0400

    3 files updated

--- Front-End/src/components/Login.jsx
+++ Front-End/src/components/Login.jsx
@@ -5,7 +5,24 @@ import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
 
+import { client } from '../client';
 const Login = () => {
+  const navigate = useNavigate(); 
+  const responseGoogle = (response) =>{
+    localStorage.setItem('user', JSON.stringify(response.profileObj))
+    const {name, googleId, imageUrl} = response.profileObj;
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl,
+    }
+    client.createIfNotExists(doc)
+      .then(() =>{
+        navigate('/', {replace: true})
+      })
+  }
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className=" relative w-full h-full">
