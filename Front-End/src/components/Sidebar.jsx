6c198c3181ac3e9deddf57100a50e821647827fb
Sebastian Infante Murguia <6dinformaticasinfante@gmail.com>
2023-05-24 16:18:07 -0400

    commi

--- Front-End/src/components/Sidebar.jsx
+++ Front-End/src/components/Sidebar.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+
+import logo from '../assets/logo.png';
+import { flares } from '../utils/data';
+
 
 const Sidebar = () => {
+  const handleCloseSidebar = () =>{
+    if(closeToggle) closeToggle(false)
+  }
+
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
+      <div className='flex flex-col'>
+
+        <Link
+            to='/'
+            className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'
+            onClick={handleCloseSidebar}>
+
+            <img src={logo} alt='logo' className='w-full' />
+          </Link>
+          <div className='flex flex-col gap-5'>
+            
+          </div>
+
+      </div>
+
+    </div>
   )
 }
 
