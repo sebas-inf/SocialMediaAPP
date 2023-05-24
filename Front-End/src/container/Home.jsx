e8b930f763495b519cf1fb8d8b6820b1fa1d9283
Sebastian Infante Murguia <6dinformaticasinfante@gmail.com>
2023-05-24 16:18:12 -0400

    coomi

--- Front-End/src/container/Home.jsx
+++ Front-End/src/container/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
 
-  useEffect(() => {
+  useEffect(() => {Sid
     const userInfo = getUserDataFromToken();
     if (userInfo) {
       const query = userQuery(userInfo.id);
@@ -40,28 +40,38 @@ const Home = () => {
       </div>
       <div className='flex md:hidden flex-row'>
         <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
+
           <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
+
           <Link to='/'>
             <img src={logo} alt='logo' className='w-28' />
           </Link>
+
           <Link to={`user-profile/${user?._id}`}>
             <img src={user?.image} alt='logo' className='w-28' />
           </Link>
+
         </div>
         { toggleSidebar ? (
           <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
             <div className='absolute w-full flex justify-end items-center p-2'>
+
               <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
+
             </div>
+
             <Sidebar user={ user } closeToggle={ setToggleSidebar } />
+
           </div>
         ) : null }
       </div>
       <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={ scrollRef }>
+
         <Routes>
           <Route path="/user-profile/:userId" element={ <UserProfile /> } />
           <Route path="/*" element={ <Posts user={ user } /> } />
         </Routes>
+
       </div>
     </div>
   );
