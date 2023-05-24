6a35265869b6ab7144771c6a4f23393d77de8300
Sebastian Infante Murguia <6dinformaticasinfante@gmail.com>
2023-05-24 15:07:36 -0400

    commit

--- Front-End/src/components/Login.jsx
+++ Front-End/src/components/Login.jsx
@@ -35,8 +35,32 @@ const Login = () => {
   };
 
   return (
-    <div>Login</div>
-  )
-}
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+          src={ shareVideo }
+          type="video/mp4"
+          loop
+          controls={ false }
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={ logo } width="130px" alt='logo' />
+          </div>
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              onSuccess={responseGoogle}
+              onError={GLoginError}
+              useOneTap
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login
--- Front-End/src/components/index.js
+++ Front-End/src/components/index.js
@@ -1,11 +1,7 @@
-export { default as CreatePin } from './CreatePost';
-export { default as Feed } from './Feed';
+
 export { default as Login } from './Login';
-export { default as MasonryLayout } from './MasonryLayout';
-export { default as Navbar } from './Navbar';
-export { default as Post } from './Post';
-export { default as PostDetail } from './PostDetail';
-export { default as Search } from './Search';
+
+
 export { default as Sidebar } from './Sidebar';
-export { default as Spinner } from './Spinner';
+
 export { default as UserProfile } from './UserProfile';
